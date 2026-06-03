5447c018a66a2196fedce2eb9f156c5ece09bdf4
DanielYanguatin <danielyanguatin021@gmail.com>
2026-06-03 16:19:42 -0500

    feat: ejercicio palindrome number resuelto

--- 09-palindrome-number.ts
+++ 09-palindrome-number.ts
@@ -1,3 +1,16 @@
 function isPalindrome(x: number): boolean {
-    
-};
+  if (x < 0) {
+    return false;
+  }
+
+  let original = x;
+  let reversed = 0;
+
+  while (x > 0) {
+    const digit = x % 10;
+    reversed = reversed * 10 + digit;
+    x = Math.floor(x / 10);
+  }
+
+  return original === reversed;
+}
