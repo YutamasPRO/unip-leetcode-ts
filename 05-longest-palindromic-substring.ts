5cd504acb786f323010ada1fc3045f53d850a9a3
Camila Arteaga <arteagacamila711@gmail.com>
2026-06-03 15:37:29 -0500

    feat: ejercicio longest palindromic substring resuelto

--- 05-longest-palindromic-substring.ts
+++ 05-longest-palindromic-substring.ts
@@ -1,3 +1,25 @@
 function longestPalindrome(s: string): string {
-    
-};
+  let start = 0;
+  let maxLength = 1;
+
+  function expandFromCenter(left: number, right: number): void {
+    while (left >= 0 && right < s.length && s[left] === s[right]) {
+      const currentLength = right - left + 1;
+
+      if (currentLength > maxLength) {
+        start = left;
+        maxLength = currentLength;
+      }
+
+      left--;
+      right++;
+    }
+  }
+
+  for (let i = 0; i < s.length; i++) {
+    expandFromCenter(i, i);
+    expandFromCenter(i, i + 1);
+  }
+
+  return s.substring(start, start + maxLength);
+}
