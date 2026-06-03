fd1a1da4f41edbde6e1e6419a4408f0e136735d2
DanielYanguatin <danielyanguatin021@gmail.com>
2026-06-03 16:07:11 -0500

    feat: ejercicio reverse integer resuelto

--- 07-reverse-integer.ts
+++ 07-reverse-integer.ts
@@ -1,3 +1,16 @@
 function reverse(x: number): number {
+  const sign = x < 0 ? -1 : 1;
 
-};
+  const reversed = parseInt(
+    Math.abs(x).toString().split("").reverse().join("")
+  ) * sign;
+
+  const MIN_INT = -(2 ** 31);
+  const MAX_INT = 2 ** 31 - 1;
+
+  if (reversed < MIN_INT || reversed > MAX_INT) {
+    return 0;
+  }
+
+  return reversed;
+}
